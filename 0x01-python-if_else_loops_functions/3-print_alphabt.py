50f02b4c7b165b4f941141bb72fca6b09b87f27f
DevOse <abuluoisereme@gmail.com>
2023-04-20 14:18:50 +0100

    tasks 2 & 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for alpha in range(97, 123):
+    if chr(alpha) == "e" or chr(alpha) == "q":
+        continue
     print(chr(alpha), end="")
-    if alpha == 101 or alpha == 113:
-        print("hi")
 
