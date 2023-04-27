9face5743b934882dd0c950f0be7bc2e6d04e51d
DevOse <abuluoisereme@gmail.com>
2023-04-27 17:26:19 +0100

    task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 
 def fizzbuzz():
     num = 1
-    while (num <= 101):
+    while (num <= 100):
         if ((num % 3 == 0) and (num % 5 == 0)):
             print("FizzBuzz", end=" ")
         elif (num % 3 == 0):
@@ -12,4 +12,4 @@ def fizzbuzz():
         else:
             print(num, end=" ")
         num = num + 1
-    print("\n", end="")
+    # print("\n", end="")
