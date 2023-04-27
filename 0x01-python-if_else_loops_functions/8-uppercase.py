6be7ee20b7157048ec2ffb12d8681fe3e5cbeaa5
DevOse <abuluoisereme@gmail.com>
2023-04-27 17:00:35 +0100

    task 8 and 9

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,27 @@
 #!/usr/bin/python3
 
+def uppercase(str):
+    upper_str = ""
+    for c in str:
+        # Check if the character is a lowercase letter
+        if ord(c) >= ord("a") and ord(c) <= ord("z"):
+            # Convert lowercase letter to uppercase by adding the ASCII offset
+            upper_char = chr(ord(c) - 32)
+        else:
+            # Otherwise, keep the uppercase character as it is
+            upper_char = c
+        # Append the uppercase characters to the uppercase string
+        upper_str += upper_char
+        # Print the uppercase string
+        print("{:s}".format(upper_char), end="")
+    # Print a new line
+    print()
 
+# OR
+# def uppercase(str):
+#     for c in str:
+#         if ord(c) >= ord("a") and ord(c) <= ord("z"):
+#             c = chr(ord(c) - 32)
+#         print("{:s}".format(c), end="")
+#     print() """
+# uppercase("oisereme")
