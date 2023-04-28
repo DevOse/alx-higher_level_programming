ef0476b287b1b41d4aa52990c18506994d1dd46e
DevOse <abuluoisereme@gmail.com>
2023-04-28 03:28:29 +0100

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+if __name__ == "__main__":
+    """Print the number of command line arguments."""
 import sys
 count = len(sys.argv) - 1
 if count == 0:
@@ -10,4 +11,4 @@ else:
     print("{} arguments".format(count))
 
 for num in range(1, len(sys.argv)):
-    print(f"{num}: {sys.argv[num]}")
+    print("{}: {}".format(num, sys.argv[num]))
