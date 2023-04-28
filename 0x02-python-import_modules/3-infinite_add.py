b46729bd6f8c16a2602fd7d41d877d6f5fbd83ee
DevOse <abuluoisereme@gmail.com>
2023-04-28 03:42:19 +0100

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 
+if __name__ == "__main__":
+    """Prints the result of addition of all arguments."""
+import sys
 
+count = 0
+
+for num in range(1, len(sys.argv)):
+    count += int(sys.argv[num])
+print("{}".format(count))
