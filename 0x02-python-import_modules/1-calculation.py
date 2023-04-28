17430ce1271345ac8a9539052f9fb3b13ad466b5
DevOse <abuluoisereme@gmail.com>
2023-04-28 02:09:10 +0100

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+import calculator_1 as calc
+a = 10
+b = 5
 
-
+print("{}".format(calc.add(a, b)))
+print("{}".format(calc.sub(a, b)))
+print("{}".format(calc.mul(a, b)))
+print("{}".format(int(calc.div(a, b))))
