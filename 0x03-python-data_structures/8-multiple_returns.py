35da7f7f94aa9fe4c72656ba8a73f49fd627b914
DevOse <abuluoisereme@gmail.com>
2023-05-10 06:04:36 +0100

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+# Function returns a tuple with the length of a string and its first character
 
-
+def multiple_returns(sentence):
+    lens = len(sentence)
+    f_char = sentence[0]
+    tup = (lens, f_char)
+    if sentence == []:
+        f_char = None
+    return tup
