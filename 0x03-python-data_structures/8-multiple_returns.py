159b9f1b991e9cda3d3293181ce539a479e65329
DevOse <abuluoisereme@gmail.com>
2023-05-10 06:09:42 +0100

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-# Function returns a tuple with the length of a string and its first character
-
+# Function that returns a tuple with the length of a string and its first character
+#If the sentence is empty, the first character should be equal to None
+#You are not allowed to import any module
 def multiple_returns(sentence):
     lens = len(sentence)
     f_char = sentence[0]
     tup = (lens, f_char)
-    if sentence == []:
+    if sentence == "":
         f_char = None
+        
     return tup
+
+sentence = "At school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
