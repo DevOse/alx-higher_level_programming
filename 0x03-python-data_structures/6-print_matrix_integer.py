20419d63bfd9d6618878e803984ba334179ae1a9
DevOse <abuluoisereme@gmail.com>
2023-05-10 05:49:16 +0100

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
 # Function that prints a matrix of integers
 
-def print_matrix_integer(matrix = [[]]):
+def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for column in row:
-            result = "".join(column)
-            print(str(result))
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-print_matrix_integer(matrix)
-print('--')
-print_matrix_integer()
+        print(" ".join("{:d}".format(column) for column in row))
